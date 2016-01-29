2351dcb9c03cc94e1451cc63a6a48c49cc20a976
martinli <youjenli1124@gmail.com>
2016-01-29 22:07:23 +0800

    調整系統主要程式的 Router 模組設計

--- src/js/system/app.js
+++ src/js/system/app.js
@@ -1,12 +1,10 @@
 /**
 * 初始化橫跨整個應用程式生命週期的路由器
 */
-define(['system/view/login', 'system/model/login',
-		'system/view/globalWidgets','system/view/mainMenu'],
+define(['./view/login', './model/login',
+		'./view/globalWidgets','./view/mainMenu'],
 	function(LoginView, LoginModule,
 			GlobalWidgets, MainMenuView){
-	
-	var pathToModuleMappings = {};
 	
 	var App = Backbone.Router.extend({
 		initialize:function(){
@@ -14,39 +12,28 @@ define(['system/view/login', 'system/model/login',
 			
 			var loginModule = new LoginModule();
 			var loginView = new LoginView({model:loginModule});
-			pathToModuleMappings["system/view/login"] = loginView;
+			self.route("system/view/login", "login", function(){
+				console.log("Route to login view.");
+				loginView.render();
+			});
 			
 			var mainMenuView = new MainMenuView();
-			pathToModuleMappings["system/view/mainMenu"]= mainMenuView;
+			self.route("system/view/mainMenu", "mainMenu", function(){
+				console.log("Route to system main menu.");
+				mainMenuView.render();
+			});
 			
 			new GlobalWidgets();
 			
 			loginModule.on("login.success", function(){
 				mainMenuView.render();
 			});
-
 			loginModule.on("logout", function(){
 				loginView.render();
 			});
 			
 			self.navigate("system/view/login", {trigger:true});
-						
 			console.log("Synnex app has been initialized.");
-		},
-		routes : {
-			"system/view/:page":"_direct"
-		},
-		_direct:function(page){
-			var routingPath = "system/view/" + page;
-			console.log("Routing path : " + routingPath);//FIXME remove this line later
-			var view = pathToModuleMappings[routingPath];
-			if (view) {
-				view.render();
-				//TODO 要加入檢查換頁是否成功的機制
-			} else {
-				console.log("Routing path : " + routingPath + " does not exists.");
-				//TODO exception handling
-			}
 		}
 	});
 	
